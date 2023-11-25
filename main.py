9641087167147beb5b647e9727569e52d98c1961
xfarkasp <xfarkasp@stuba.sk>
2023-11-25 10:57:08 +0100

    Connection establishment test

--- main.py
+++ main.py
@@ -4,6 +4,71 @@ import struct
 import threading
 import time
 
+def createConnection(filename, host, port):
+    try:
+            # Create a socket for communication
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                # Connect to the server
+                s.connect((host, port))
+
+                # Send the file size and any other metadata in the header
+                header = struct.pack('!B', 0)
+                s.sendall(header)
+                print("Syn sent.")
+
+                # Bind the socket to a specific address and port
+                s.bind((host, port))
+
+                # Listen for incoming connections
+                s.listen()
+
+                print("Waiting for a connection...")
+
+                # Accept a connection from a client
+                conn, addr = s.accept()
+                with conn:
+
+                    # Receive the header containing file size
+                    header = conn.recv(8)
+                    type = struct.unpack('!B', header)[0]
+                    if type == 1:
+                        print("connection established")
+
+    except ConnectionRefusedError:
+        print(f"Connection refused from the host: " + host)
+
+def waitForSyn(host, port):
+    try:
+            # Create a socket for communication
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                # Connect to the server
+                s.connect((host, port))
+
+                # Bind the socket to a specific address and port
+                s.bind((host, port))
+
+                # Listen for incoming connections
+                s.listen()
+
+                print("Waiting for a connection...")
+
+                # Accept a connection from a client
+                conn, addr = s.accept()
+                with conn:
+                    # Receive the header containing file size
+                    header = conn.recv(8)
+                    type = struct.unpack('!B', header)[0]
+                    if(type == 0):
+                        print("SYN RECIEVED FORM: " + host)
+                        # Send the file size and any other metadata in the header
+                        header = struct.pack('!B', 1)
+                        s.sendall(header)
+                        print("ACK to SYN from: " + host +" sent.")
+
+    except ConnectionRefusedError:
+        print(f"Connection refused from the host: " + host)
+
+
 def send_file(filename, host, port):
     try:
         # Open the file in binary mode
@@ -33,6 +98,9 @@ def send_file(filename, host, port):
         print("File sent successfully.")
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
+
+    except ConnectionRefusedError:
+        print(f"Connection refused from the host: " + host)
 
 def receive_file(filename, host, port):
     # Create a socket for communication
@@ -80,7 +148,12 @@ def keep_alive_thread(s, interval):
 
 # Example usage:
 if __name__ == "__main__":
+    # host = 'localhost'
+    host = '192.168.1.14'
+    port = 12345;
+
     while(1):
+        print("0 = set up config")
         print("1 = start connection")
         print("2 = send text")
         print("3 = send file")
@@ -88,23 +161,43 @@ if __name__ == "__main__":
         print("5 = change fragment size")
 
         user_input = input("Select function: ")
+        print(user_input)
+        if user_input == '0':
+            #host = int(input("Input reciever ip: "))
+            port = int(input("Select port to operate on: "))
+            folder = input("Path to save data: ")
 
-    # host = 'localhost'
-    host = '192.168.11.136'
-    port = 12345;
-    # Server (receiver) side
-    receiver_thread = threading.Thread(target=receive_file, args=('C:\\Users\\lordp\\OneDrive\\Documents\\AkademickaPoda\\2.Rok\\received_file.txt', host, port));
-    receiver_thread.start();
+            # Server (receiver) side
+            receiver_thread = threading.Thread(target=receive_file, args=('C:\\Users\\pedro\\PycharmProjects\\pks2\\', host, port));
+            receiver_thread.start();
+
+        if user_input == '1':
+            # Server (receiver) side
+            receiver_thread = threading.Thread(target=receive_file,
+                                               args=('C:\\Users\\pedro\\PycharmProjects\\pks2\\', host, port));
+            receiver_thread.start();
+        receiver_thread = threading.Thread(target=receive_file,
+                                           args=('C:\\Users\\pedro\\PycharmProjects\\pks2\\', host, port));
+        receiver_thread.start();
+        if user_input == '3':
+            host = input("Input reciever ip: ")
+            port = int(input("Select port to operate on: "))
+            folder = input("Path to send data: ")
+
+            # Server (receiver) side
+            sender_thread = threading.Thread(target=send_file, args=(
+            'C:\\Users\\pedro\\PycharmProjects\\pks2\\piculkovia.jpeg', host, port));
+            sender_thread.start();
 
     # Client (sender) side with keep-alive thread
-    sender_thread = threading.Thread(target=send_file, args=('C:\\Users\\lordp\\OneDrive\\Documents\\AkademickaPoda\\2.Rok\\3.ZS\\OS\\du\\du2_Farkas.txt', host, port));
+    #
     # keep_alive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # keep_alive_socket.connect((host, port))  # Connect to the server
     # keep_alive_thread = threading.Thread(target=keep_alive_thread, args=(keep_alive_socket, 5))  # 5 seconds interval
-    sender_thread.start();
+    #sender_thread.start();
     # keep_alive_thread.start()
 
     # Wait for both threads to finish
-    sender_thread.join();
+    #
     # keep_alive_thread.join()
-    receiver_thread.join();
+
